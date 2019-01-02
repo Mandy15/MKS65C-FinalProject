d4a39092c02eca9abc3ce40ea4aeaf79ee0d5929
Mandy <mmai1@stuy.edu>
2019-01-02 15:26:54 -0500

    update

--- makefile
+++ makefile
@@ -5,7 +5,7 @@ word_association.o: word_association.c word_association.h
 	gcc -c word_association.c
 
 clean:
-	rm word_association word_association.o
+	rm word_association *.o
 
 run:
 	./word_association
--- word_association.c
+++ word_association.c
@@ -1,14 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <errno.h>
 #include "word_association.h"
 
 int main(){
+  printf("WELCOME TO MANDY'S WORD ASSOCIATION GAME!!!\n");
+
   return 0;
 }
