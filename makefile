1464d9497651c2ac5afcfc88abb830453522a68b
Mandy <mmai1@stuy.edu>
2019-01-02 15:17:06 -0500

    setup

---
+++ makefile
@@ -0,0 +1,11 @@
+all: word_association.o
+	gcc -o word_association word_association.o
+
+word_association.o: word_association.c word_association.h
+	gcc -c word_association.c
+
+clean:
+	rm word_association word_association.o
+
+run:
+	./word_association
---
+++ word_association.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include "word_association.h"
+
+int main(){
+  return 0;
+}
